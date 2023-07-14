c0ed290c6c549c77f328fa3b275f90d92be97fe2
Routash Kumar <routashsain@gmail.com>
2023-07-14 18:06:03 +0530

    errorasdf

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 process.on("uncaughtException", (err) => {
-  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
-  console.log(err.name, err.message);
-  process.exit(1);
+  // console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  // console.log(err.name, err.message);
+  // process.exit(1);
 });
 mongoose.set('strictQuery', false);
 
@@ -27,10 +27,10 @@ const server = app.listen(port, () => {
 });
 
 process.on("unhandledRejection", (err) => {
-  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
+  // console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  // console.log(err.name, err.message);
+  // server.close(() => {
+  //   process.exit(1);
+  // });
 });
 ``;
