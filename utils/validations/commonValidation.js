51c86f3f6db3a233fc00cfb31163e5b53ccf0d9b
Routash Kumar <routashsain@gmail.com>
2023-07-14 17:31:06 +0530

    aaaa

--- utils/validations/commonValidation.js
+++
@@ -1,7 +0,0 @@
-const customValidateForStatus = (value, helper) => {
-  if (value === "user_admin" || value === "admin") {
-    return value;
-  }
-  return helper.error("please enter valid position type user_admin/user");
-};
-module.exports.customValidateForStatus = customValidateForStatus;
