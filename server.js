bb9477102846b3a5183d29e3e387f3e5d4ab51a0
Routash Kumar <routashsain@gmail.com>
2023-07-14 17:50:23 +0530

    asdf

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ process.on("uncaughtException", (err) => {
   console.log(err.name, err.message);
   process.exit(1);
 });
+mongoose.set('strictQuery', false);
 
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
